a789bcc523be408aa67a34337ca3501025916114
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-02-07 19:33:31 +0900

    remove blood

--- app/controllers/humen_controller.rb
+++ app/controllers/humen_controller.rb
@@ -62,10 +62,6 @@ class HumenController < ApplicationController
     end
   end
 
-  def blood
-    @human = Human.find(params[:human_id])
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_human
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :humen do
-    get 'blood'
-  end
+  resources :humen
   resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
