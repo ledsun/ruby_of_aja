3c64795618ee7aa8bf46b623f0c132d12dde6a09
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-02-13 00:14:54 +0900

    refactor: Extract a Eval module

---
+++ app/lib/eval.rb
@@ -0,0 +1,46 @@
+module Eval
+  def self.random_gets
+    source = %w(佐藤 鈴木 高橋 田中 伊藤 渡辺 山本 中村 小林 加藤 吉田 AAA 5)
+    sample = []
+    stdin = Object.new
+    stdin.singleton_class.instance_eval do
+      define_method 'gets' do
+        val = source.sample
+        sample << val
+        val
+      end
+    end
+
+    [stdin, sample]
+  end
+
+  def self.repeat_gets(sample)
+    stdin = Object.new
+    stdin.singleton_class.instance_eval do
+      define_method 'gets' do
+        sample.shift
+      end
+    end
+
+    stdin
+  end
+
+  def self.wrap_eval(stdin, ruby_script)
+    begin
+      $stdin = stdin
+      $stdout = StringIO.new
+      eval ruby_script
+      $stdout.string
+    rescue => e
+      logger.debug e
+      bc = ActiveSupport::BacktraceCleaner.new
+      bc.add_silencer { |line| line =~ %r{gems} }
+      logger.debug bc.clean(e.backtrace).join("\n")
+      logger.debug ruby_script
+      raise
+    ensure
+      $stdin = STDIN
+      $stdout = STDOUT
+    end
+  end
+end
--- app/models/human.rb
+++ app/models/human.rb
@@ -14,62 +14,18 @@ class Human < ApplicationRecord
   end
 
   def input
-    stdin, @sample = random_gets
-    wrap_eval stdin
+    stdin, @sample = Eval.random_gets
+    Eval.wrap_eval stdin, solution
     @sample
   end
 
   def result
-    wrap_eval repeat_gets(@sample)
+    Eval.wrap_eval Eval.repeat_gets(@sample), solution
   end
 
   private
 
   def markdown
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
-  end
-
-  def random_gets
-    sample = []
-    stdin = Object.new
-    stdin.singleton_class.instance_eval do
-      define_method 'gets' do
-        val = %w(佐藤 鈴木 高橋 田中 伊藤 渡辺 山本 中村 小林 加藤 吉田 AAA 5).sample
-        sample << val
-        val
-      end
-    end
-
-    [stdin, sample]
-  end
-
-  def repeat_gets(sample)
-    stdin = Object.new
-    stdin.singleton_class.instance_eval do
-      define_method 'gets' do
-        sample.shift
-      end
-    end
-
-    stdin
-  end
-
-  def wrap_eval stdin
-    begin
-      $stdin = stdin
-      $stdout = StringIO.new
-      eval solution
-      $stdout.string
-    rescue => e
-      logger.debug e
-      bc = ActiveSupport::BacktraceCleaner.new
-      bc.add_silencer { |line| line =~ %r{gems} }
-      logger.debug bc.clean(e.backtrace).join("\n")
-      logger.debug solution
-      raise
-    ensure
-      $stdin = STDIN
-      $stdout = STDOUT
-    end
   end
 end
